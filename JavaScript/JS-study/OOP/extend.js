34b67bf91a5dbfeedbab67c3ec381c8e83783336
NARUTOne <wznaruto326@163.com>
2018-08-13 23:34:15 +0800

    extends

---
+++ JavaScript/JS-study/OOP/extend.js
@@ -0,0 +1,33 @@
+/*
+ * @File: extend.js
+ * @Project: OOP
+ * @File Created: Monday, 13th August 2018 11:19:20 pm
+ * @Author: NARUTOne (wznaruto326@163.com/wznarutone326@gamil.com)
+ * -----
+ * @Last Modified: Monday, 13th August 2018 11:19:32 pm
+ * @Modified By: NARUTOne (wznaruto326@163.com/wznarutone326@gamil.com>)
+ * -----
+ * @Copyright <<projectCreationYear>> - 2018 bairong, bairong
+ * @fighting: code is far away from bug with the animal protecting
+ *  ┏┓　 ┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　┃   
+ * ┃　 ━　 ━　  ┃     
+ * ┃　┳┛　┗┳　 ┃      
+ * ┃　　　　　　　┃   
+ * ┃　　　┻　　　┃    
+ * ┃　　　　　　　┃   
+ * ┗━┓　　　 ┏━┛       
+ * 　　┃　　　┃ 护码神兽
+ * 　　┃　　　┃ 代码零BUG！
+ * 　　┃　　　┗━━━┓   
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛━ ━  🚀🚀🚀
+ * 　　┗┓┓┏━┳┓┏┛  ━    
+ * 　　　┃┫┫　┃┫┫     
+ * 　　　┗┻┛　┗┻┛     
+ */
+
+/**
+ * ! 继承 
+ */ 
