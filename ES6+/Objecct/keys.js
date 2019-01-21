50d39ed77eedaa018158a9aa556000ef27e5f543
NARUTOne <wznaruto326@163.com>
2019-01-21 11:38:29 +0800

    移动drag，及Object.keys

--- ES6+/Objecct/keys.js
+++ ES6+/Objecct/keys.js
@@ -1,6 +1,7 @@
 // ! object.keys()
-function keys(object) { var result, key
-  result = [];
+
+function keys(object) { 
+  var result, key, result = [];
   for (key in object){
       if (object.hasOwnProperty(key))  result.push(key)
   }
